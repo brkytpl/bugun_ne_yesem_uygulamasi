e7dc0da625b73bac5517d2be2c1cdff159a4bea0
Berkay Topal <brkytpl@gmail.com>
2024-03-09 19:16:09 +0300

    ne yesem uygulaması tamamlandı

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(BenimUygulamam());
@@ -23,7 +24,24 @@ class BenimUygulamam extends StatelessWidget {
   }
 }
 
-class YemekSayfasi extends StatelessWidget {
+class YemekSayfasi extends StatefulWidget {
+  @override
+  State<YemekSayfasi> createState() => _YemekSayfasiState();
+}
+
+class _YemekSayfasiState extends State<YemekSayfasi> {
+  int corbaNo = 1;
+  int yemekNo = 1;
+  int tatliNo = 1;
+
+  void yemekeleriYenile() {
+    setState(() {
+      corbaNo = Random().nextInt(5) + 1;
+      yemekNo = Random().nextInt(5) + 1;
+      tatliNo = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -31,23 +49,35 @@ class YemekSayfasi extends StatelessWidget {
         children: [
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Image.asset('assets/corba_1.jpg'),
+              padding: const EdgeInsets.all(60.0),
+              child: FloatingActionButton(
+                  backgroundColor: Colors.yellow,
+                  splashColor: Colors.black,
+                  onPressed: yemekeleriYenile,
+                  child: Image.asset('assets/corba_$corbaNo.jpg')),
             ),
           ),
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Image.asset('assets/yemek_1.jpg'),
+              padding: const EdgeInsets.all(60.0),
+              child: FloatingActionButton(
+                  backgroundColor: Colors.yellow,
+                  splashColor: Colors.black,
+                  onPressed: yemekeleriYenile,
+                  child: Image.asset('assets/yemek_$yemekNo.jpg')),
             ),
           ),
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Image.asset('assets/tatli_1.jpg'),
+              padding: const EdgeInsets.all(60.0),
+              child: FloatingActionButton(
+                  backgroundColor: Colors.yellow,
+                  splashColor: Colors.black,
+                  onPressed: yemekeleriYenile,
+                  child: Image.asset('assets/tatli_$tatliNo.jpg')),
             ),
           ),
-        ],
+        ], // Children
       ),
     );
   }
